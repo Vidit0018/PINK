0bd340b67d5b215667722048cb2e95d54fa760de
Vidit Prajapati <vidit.msit@gmail.com>
2024-02-24 20:36:40 +0530

    login router connected

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {login,home,contact} =require("../controllers/maincontroller")
-router.get("/login", login);
+router.get("/", login);
 router.get("/home", home);
 router.get("/contact", contact);
 module.exports= router;
