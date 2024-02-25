b07c7ed493b543cee1d7bef5ec83cd100a735fb5
Vidit Prajapati <vidit.msit@gmail.com>
2024-02-25 13:30:12 +0530

    index changed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {login,home,contact,userprofile,signup_post,login_post} =require("../controllers/maincontroller")
-router.get("/login", login);
+router.get("/", login);
 router.get("/home", home);
 router.get("/contact", contact);
 router.post("/signup",signup_post);
