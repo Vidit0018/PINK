3e32213215e5e998b88322710f0c6f8bce0a3b76
Vidit Prajapati <vidit.msit@gmail.com>
2024-02-26 00:56:21 +0530

    donation page created

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {login,home,contact,userprofile,signup_post,login_post,volunteer} =require("../controllers/maincontroller")
+const {login,home,contact,userprofile,signup_post,login_post,volunteer,donation,donation_form} =require("../controllers/maincontroller")
 router.get("/", login);
 router.get("/home", home);
 router.get("/contact", contact);
@@ -8,5 +8,7 @@ router.post("/signup",signup_post);
 router.post("/login",login_post);
 router.get("/userprofile", userprofile);
 router.get("/volunteer", volunteer);
+router.get("/donation", donation);
+router.get("/donation_form", donation_form);
 // router.get("/userprofile/:id",userprofile);
 module.exports= router;
