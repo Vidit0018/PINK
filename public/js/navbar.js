7c871a39164dd23d3ca851a2d279a618ca0b2608
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2024-02-26 14:41:24 +0530

    home updated

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -11,26 +11,7 @@ var more = document.getElementById("more");
 
 if (window.scrollY > 50) {
     nav.classList.add('affix');
-    logo.classList.add("white");
-    home.classList.add("white")
-    home.classList.remove("pink");
-    about.classList.add("white");
-    about.classList.remove("pink");
-    contact.classList.add("white");
-    contact.classList.remove("pink");
-    more.classList.add("white");
-    more.classList.remove("pink");
 } else {
     nav.classList.remove('affix');
-    logo.classList.remove("white");
-    home.classList.add("pink");
-    home.classList.remove("white");
-    about.classList.add("pink");
-    about.classList.remove("white");
-    contact.classList.add("pink");
-    contact.classList.remove("white");
-    more.classList.add("pink");
-    more.classList.remove("white");
-    
 }
 });
