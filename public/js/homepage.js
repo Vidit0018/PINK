f20c4dba68afbb797ab3fe00b649a1770119210d
Gautam Sachdeva <gautamsachdeva20@gmail.com>
2024-02-28 19:35:03 +0530

    news added

---
+++ public/js/homepage.js
@@ -0,0 +1,53 @@
+const API_KEY = "aad850529a4540ceb99d4ce95436ad1d";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => {
+    fetchNews("Breast Cancer");
+});
+
+function reload(){
+    window.location.reload();
+}
+
+async function fetchNews(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles){
+    const cardsContainer = document.getElementById("news-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach(article => {
+        if (!article.urlToImage) return;
+
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article){
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.setAttribute("src", article.urlToImage);
+    newsTitle.textContent = article.title;
+    newsDesc.textContent = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    });
+
+    newsSource.textContent = `${article.source.name} • ${date}`;
+
+    cardClone.firstElementChild.style.cursor = "pointer";
+    cardClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"blank");
+    })
+}
