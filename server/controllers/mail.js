d54e5c11c7e89d17bcd90f2f54a748e6d4c9e5ae
mohitarora8181 <mannyisha808@gmail.com>
2024-03-03 19:22:16 +0530

    check env

--- server/controllers/mail.js
+++ server/controllers/mail.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const nodemailer = require("nodemailer");
 const {google }= require('googleapis');
 const CLIENT_ID = '110051534542-c2jp5rlgdnf15kgl7a9luekmefqqf79d.apps.googleusercontent.com'
 
 const REDIRECT_URI ='https://developers.google.com/oauthplayground'
 const REFRESH_TOKEN ='1//04YOg1pX3QuX-CgYIARAAGAQSNwF-L9IrDKx4NOXRpVpT_PvNItNZp4WSm9ZFqkLfXCiGPNQgqPt0GE__74e8lkZvunBFdZt5uFM'
-const CLIENT_SECRET ='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'
+const CLIENT_SECRET = process.env.CLIENT_SECRET
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
 
@@ -25,7 +26,7 @@ async function sendMail(params) {
         })
         const mailOptions = {
             from: 'Team Pinkaware <pinkaware007@gmail.com>',
-            to: ' mannyisha808@gmail.com ,vidit.msit@gmail.com',
+            to: ' mannyisha808@gmail.com',
             subject: "Appointment registered",
             text: `Dear ${params.Username},\n\nWe hope this email finds you well.\nWe are writing to confirm that your registration for an appointment has been successfully processed today. We are delighted to have you scheduled and look forward to providing you with our exceptional services.\nHere are the details of your appointment:\n\nDate: [Date that customer choose]\nTime: [Time of Appointment]\nVenue: [Venue Name]\nService/Reason for Appointment: [Brief description]\n\nPlease make sure to arrive at the venue on time and bring any necessary documents or information required for your appointment. If you have any specific requirements or questions, please feel free to reach out to us at [Contact Information].\n\nWe strive to ensure that your experience with us is smooth and satisfactory. \nYour feedback is valuable to us, and we appreciate any comments or suggestions you may have regarding our services.\nThank you for choosing PinkAware. We are committed to providing you with the best possible experience.`,
 
