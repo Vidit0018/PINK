6cbdcd6f2952d1adffe131e7ad6328ffc3fc8dab
vidit0018 <vidit.msit@gmail.com>
2025-02-19 02:01:21 +0530

    refresh token changed

--- server/controllers/mail.js
+++ server/controllers/mail.js
@@ -4,7 +4,7 @@ const {google }= require('googleapis');
 const CLIENT_ID = '110051534542-c2jp5rlgdnf15kgl7a9luekmefqqf79d.apps.googleusercontent.com'
 
 const REDIRECT_URI ='https://developers.google.com/oauthplayground'
-const REFRESH_TOKEN ='1//04YOg1pX3QuX-CgYIARAAGAQSNwF-L9IrDKx4NOXRpVpT_PvNItNZp4WSm9ZFqkLfXCiGPNQgqPt0GE__74e8lkZvunBFdZt5uFM'
+const REFRESH_TOKEN ='1//04jPE381L3H5UCgYIARAAGAQSNwF-L9IrRtVNn2PM0u-2mlnNzqBcrv2LEl2xjAP6r1DwKGFmu6_Z9W1v0C3dVYvJi9NuWU4x3f0'
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET, REDIRECT_URI)
 oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
